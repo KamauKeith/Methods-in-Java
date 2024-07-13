6df1874eccae7a5dab8d158f84509d923b4ef2f2
Keith Kamau <2301603@students.kcau.ac.ke>
2024-07-13 12:04:06 +0300

    Done

---
+++ Java Methods/Methods.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter The First Number: ");
+    int fnum = scanner.nextInt();
+
+    System.out.println("Enter The Second Number: ");
+    int snum = scanner.nextInt();
+
+    System.out.println("Enter The Third Number: ");
+    int tnum = scanner.nextInt();
+
+    int largest;
+    if (fnum >= snum && fnum >= tnum) {
+        largest = fnum;
+    } else if (snum >= fnum && snum >= tnum) {
+        largest = snum;
+    } else {
+        largest = tnum;
+    }
+    
+    // Determining the smallest number
+    int smallest;
+    if (fnum <= snum && fnum <= tnum) {
+        smallest = fnum;
+    } else if (snum <= fnum && snum <= tnum) {
+        smallest = snum;
+    } else {
+        smallest = tnum;
+    }
+    
+    System.out.println("The smallest number: " + smallest);
+        System.out.println("The largest number: " + largest);
+        System.out.println(largest + " is your largest and " + smallest + " smallest number.");
+
+        scanner.close();
+
+}
